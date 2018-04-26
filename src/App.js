59ba92893d8cb6b3b5cbb6ecd1057814c3951ccf
Debojit Kaushik <kaushik.debojit@gmail.com>
2018-04-26 04:03:31 -0500

    Partcile js and some styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import BubbleChart from '@weknow/react-bubble-chart-d3';
 import logo from './Logo.png';
 import './App.css';
+import Particles from 'react-particles-js';
 import  {
   LineChart, 
   Line, 
@@ -21,7 +22,8 @@ import  {
   PolarAngleAxis, 
   PolarRadiusAxis
 } from 'recharts';
-// import q from './q10-da.js'
+
+let background_url = require('./shots.jpg');
 var q = require('./q10-d.json');
 var q1 = require('./q1-d.json');
 var q8 = require('./q8-d.json');
@@ -67,10 +69,10 @@ data6_2 = []
 for (var i = 0; i < Object.keys(q6_2).length; i++) {
   let d = {};
   d.label = Object.keys(q6_2)[i];
-  d.value = Object.keys(q6_2)[i][1];
-  // console.log(Object.keys(q6_2)[i])
+  d.value = q6_2[Object.keys(q6_2)[i]][1];
   data6_2.push(d);
 }
+
 
 for(var i = 1; i <= 12; i++){
   let d = {};
@@ -122,131 +124,158 @@ const renderTooltipContent = (o) => {
   );
 };
 
+
+// 41.874306, -87.647175
+
 class App extends Component {
+
+  setMarker = () => {
+    var uluru = { lat: 41.874306, lng: -87.647175 };
+    var marker = new window.google.maps.Marker({
+      position: uluru,
+      map: window.gmaps['map1'].gmap
+    });
+  };
+
   render() {
+    let mapOptions = {
+      zoom: 15,
+      center: { lat: 41.874306, lng: -87.647175 }
+    };
     return (
-      <div className="App">
-        <header>
-          <img src={logo} style={styles.logo} alt="logo" />
-          <h1 className="App-title">Alethea</h1>
-        </header>
-        <h1 className="App-intro">
-          Visualizations!
-        </h1>
-        <div>
-          <p>
-            Line Chart representation of monthly probabilities of every crime
-          </p>
-        </div>
+      <div>
+        <Particles style = {styles.particles} 
+        />
+        <div className="App" style = {styles.parentBox}>
+          <header>
+            <img src={logo} style={styles.logo} alt="logo" />
+            <h1>Alethea</h1>
+          </header>
+          <h2>
+            Visualizations!
+          </h2>
+          {/* Line Chart */}
+          <div style = {styles.centerBox} class = "card">
+            <div style={styles.divBoxes}>
+              <p style = {{color: 'black'}}>Line Chart representation of monthly probabilities of every crime</p>
+              <LineChart width={800} height={250} data = {data}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="mvt" stroke="#8884d8" />
+                <Line type="monotone" dataKey="THEFT" stroke="#82ca9d" />
+                <Line type="monotone" dataKey="ROBBERY" stroke="#000000" />
+                <Line type="monotone" dataKey="BURGLARY" stroke="#900" />
+              </LineChart>
+            </div>
+            {/* Bar Chart */}
+            <div style = {styles.divBoxes}>
+              <p style = {{color: 'black'}}>Bar Chart representation of monthly probabilities of every crime</p>
+              <BarChart height = {250} width = {800} data = {data}
+              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+              <CartesianGrid strokedDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="mvt" fill="#8884d8" />
+              <Bar dataKey="THEFT" fill="#82ca9d" />
+              <Bar dataKey="ROBBERY" fill="#000000" />
+              <Bar dataKey="BURGLARY" fill="#900" />
+              </BarChart>
+            </div>
 
-        {/* Line Chart */}
-        <LineChart width={800} height={250} data = {data}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="mvt" stroke="#8884d8" />
-          <Line type="monotone" dataKey="THEFT" stroke="#82ca9d" />
-          <Line type="monotone" dataKey="ROBBERY" stroke="#000000" />
-          <Line type="monotone" dataKey="BURGLARY" stroke="#900" />
-        </LineChart>
-        <br/>
+            <div style = {styles.divBoxes}>
+            <p style = {{color: 'black'}}>Bar Chart representation of number of restaurants open after x years of failed inspection</p>
+            <BarChart height = {250} width = {800} data = {data8}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokedDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" fill="#900" />
+            </BarChart>
+            </div>
 
-          {/* Bar Chart */}
-          <p>Bar Chart representation of monthly probabilities of every crime</p>
-          <BarChart height = {250} width = {800} data = {data}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-          <CartesianGrid strokedDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="mvt" fill="#8884d8" />
-          <Bar dataKey="THEFT" fill="#82ca9d" />
-          <Bar dataKey="ROBBERY" fill="#000000" />
-          <Bar dataKey="BURGLARY" fill="#900" />
-          </BarChart>
-        <br/>
-        {/* Bar Chart */}
-          <p>Bar Chart representation of number of restaurants open after x years of failed inspection</p>
-          <BarChart height = {250} width = {800} data = {data8}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-          <CartesianGrid strokedDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="count" fill="#900" />
-          </BarChart>
-          <br />
-          <p>Area Chart representation of type of establishment with percentage of crimes.</p>
-          <AreaChart width={800} height={400} data={data2} stackOffset="expand"
-            margin={{top: 10, right: 30, left: 0, bottom: 0}} >
-            <XAxis dataKey="Year"/>
-            <YAxis tickFormatter={toPercent}/>
-            <Tooltip/>
-            <Area type='monotone' dataKey='a' stackId="1" stroke='#8884d8' fill='#8884d8' />
-            <Area type='monotone' dataKey='b' stackId="1" stroke='#82ca9d' fill='#82ca9d' />
-            <Area type='monotone' dataKey='c' stackId="1" stroke='#ffc658' fill='#ffc658' />
-          </AreaChart>
-          <br />
-          <p>Composed Charts representation of Range of liquor licenses with Crimes taking place</p>
-          <ComposedChart width={800} height={400} data={data9}
-            margin={{top: 20, right: 20, bottom: 20, left: 20}}>
-              <CartesianGrid stroke='#f5f5f5'/>
-              <XAxis dataKey="name"/>
-              <YAxis />
-              <Tooltip/>
-              <Legend/>
-              <Bar dataKey='Crimes' barSize={20} fill='#413ea0'/>
-              <Line type='monotone' dataKey='Crimes' stroke='#ff7300'/>
-          </ComposedChart>
-          <RadarChart cx={300} cy={250} outerRadius={200} width={600} height={500} data={data6}>
-            <PolarGrid />
-            <PolarAngleAxis dataKey="name" />
-            <PolarRadiusAxis angle={30} domain={[0, 8000]}/>
-            <Radar name="Positive" dataKey="positive" stroke="#8884d8" fill="#8884d8" fillOpacity={0.5}/>
-            <Legend />
-          </RadarChart>
-          <RadarChart cx={300} cy={250} outerRadius={200} width={600} height={500} data={data6}>
-            <PolarGrid />
-            <PolarAngleAxis dataKey="name" />
-            <PolarRadiusAxis angle={30} domain={[0, 4000]}/>
-            <Radar name="Negative" dataKey="negative" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.5}/>
-            <Legend />
-          </RadarChart>
-          <div>
-          <BubbleChart
-                graph = {{
-                  zoom: 1.1,
-                  offsetX: -0.05,
-                  offsetY: -0.01,
-                }}
-                width={1000}
-                height={800}
-                showLegend={true} // optional value, pass false to disable the legend.
-                legendPercentage={20} // number that represent the % of with that legend going to use.
-                legendFont={{
-                      family: 'Arial',
-                      size: 12,
-                      color: '#000',
-                      weight: 'bold',
+            <div style = {styles.divBoxes}>
+              <p style = {{color: 'black'}}>Area Chart representation of type of establishment with percentage of crimes.</p>
+              <AreaChart width={800} height={400} data={data2} stackOffset="expand"
+                margin={{top: 10, right: 30, left: 0, bottom: 0}} >
+                <XAxis dataKey="Year"/>
+                <YAxis tickFormatter={toPercent}/>
+                <Tooltip/>
+                <Area type='monotone' dataKey='a' stackId="1" stroke='#8884d8' fill='#8884d8' />
+                <Area type='monotone' dataKey='b' stackId="1" stroke='#82ca9d' fill='#82ca9d' />
+                <Area type='monotone' dataKey='c' stackId="1" stroke='#ffc658' fill='#ffc658' />
+              </AreaChart>
+            </div>
+
+            <div style = {styles.divBoxes}>
+              <p style = {{color: 'black'}}>Composed Charts representation of Range of liquor licenses with Crimes taking place</p>
+              <ComposedChart width={800} height={400} data={data9}
+                margin={{top: 20, right: 20, bottom: 20, left: 20}}>
+                  <CartesianGrid stroke='#f5f5f5'/>
+                  <XAxis dataKey="name"/>
+                  <YAxis />
+                  <Tooltip/>
+                  <Legend/>
+                  <Bar dataKey='Crimes' barSize={20} fill='#413ea0'/>
+                  <Line type='monotone' dataKey='Crimes' stroke='#ff7300'/>
+              </ComposedChart>
+            </div>
+            <div style = {styles.divBoxes}>
+              <RadarChart cx={300} cy={250} outerRadius={200} width={600} height={500} data={data6}>
+                <PolarGrid />
+                <PolarAngleAxis dataKey="name" />
+                <PolarRadiusAxis angle={30} domain={[0, 8000]}/>
+                <Radar name="Positive" dataKey="positive" stroke="#8884d8" fill="#8884d8" fillOpacity={0.5}/>
+                <Legend />
+              </RadarChart>
+            
+              <RadarChart cx={300} cy={250} outerRadius={200} width={600} height={500} data={data6}>
+                <PolarGrid />
+                <PolarAngleAxis dataKey="name" />
+                <PolarRadiusAxis angle={30} domain={[0, 4000]}/>
+                <Radar name="Negative" dataKey="negative" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.5}/>
+                <Legend />
+              </RadarChart>
+            </div>
+            <div style = {[styles.centerBox, styles.divBoxes]}>
+              <BubbleChart
+              style = {[styles.centerBox, styles.divBoxes]}
+                    graph = {{
+                      zoom: 1.1,
+                      offsetX: -0.05,
+                      offsetY: -0.01,
                     }}
-                valueFont={{
-                      family: 'Arial',
-                      size: 12,
-                      color: '#fff',
-                      weight: 'bold',
-                    }}
-                labelFont={{
-                      family: 'Arial',
-                      size: 16,
-                      color: '#fff',
-                      weight: 'bold',
-                    }}
-                data={data6_2}
-              />
+                    width={900}
+                    height={800}
+                    showLegend={true} // optional value, pass false to disable the legend.
+                    legendPercentage={20} // number that represent the % of with that legend going to use.
+                    legendFont={{
+                          family: 'Arial',
+                          size: 12,
+                          color: '#000',
+                          weight: 'bold',
+                        }}
+                    valueFont={{
+                          family: 'Arial',
+                          size: 12,
+                          color: '#fff',
+                          weight: 'bold',
+                        }}
+                    labelFont={{
+                          family: 'Arial',
+                          size: 16,
+                          color: '#fff',
+                          weight: 'bold',
+                        }}
+                    data={data6_2} />
+            </div>
           </div>
+        </div>
       </div>
     );
   }
@@ -257,6 +286,48 @@ const styles = {
   logo: {
     height: '60px',
     width: '60px'
+  },
+  divBoxes: {
+    boxShadow: '2px 2px 10px #888886',
+    margin: '10px',
+    borderRadius: '3px',
+    backgroundColor: 'white',
+    width: '75%',
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  parentBox: {
+    backgroundSize: 'inherit',
+    overflow: 'hidden',
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '10px',
+    zIndex: '1000',
+    color: 'white'
+  },
+  centerBox: {
+    padding: '10px',
+    width: '85%',
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  particles: {
+    backgroundColor: '#000000',
+    position: 'fixed',
+    top: '0',
+    right: '0',
+    bottom: '0',
+    left: '0',
+    zIndex: '-1000' 
   }
 }
 
